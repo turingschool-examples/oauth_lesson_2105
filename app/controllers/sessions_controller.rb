93557edf102cc2a33f93cb4128a89539c6b7bd1a
Meg Stang <mstang15@gmail.com>
2021-09-15 11:42:26 -0600

    Initial Commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController <ApplicationController 
+    def create 
+        user = User.find_or_create_by(email: user_params[:email])
+        user.update(user_params)
+
+        session[:user_id] = user.id 
+
+        redirect_to '/dashboard'
+    end 
+
+    private 
+
+    def auth_hash
+        request.env['omniauth.auth']
+    end 
+
+    def user_params
+        {google_id: auth_hash['uid'],
+        email: auth_hash['info']['email'],
+        token: auth_hash['credentials']['token']}
+    end 
+end 
